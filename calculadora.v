9b04699d509097f6088c4c46df08b416fbed1344
Zazamartins <isaacmrtnsss@gmail.com>
2025-04-14 10:19:03 -0300

    Criado Código da Calculadora sem registradores

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,11 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+  assign saida = (codigo == 3'b000) ? 8'b00000000 :  // Zerar
+                 (codigo == 3'b001) ? entrada_A  :  // Mostrar A
+                 (codigo == 3'b010) ? entrada_B  :  // Mostrar B
+                 (codigo == 3'b011) ? (entrada_A + entrada_B) : // Somar
+                 (codigo == 3'b100) ? (entrada_A - entrada_B) : // Subtrair
+                 8'b00000000; // Operação inválida retorna ZERO
 
 endmodule
